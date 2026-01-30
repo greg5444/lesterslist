7672901818cacebd567a278f7095b8047dfeedb9
greg5444 <52467789+greg5444@users.noreply.github.com>
2026-01-29 23:55:51 -0600

    Delete schema.sql

--- schema.sql
+++
@@ -1,9 +0,0 @@
--- MySQL Schema for Lesterslist
-CREATE TABLE IF NOT EXISTS verification_status(
-  RecordID INT AUTO_INCREMENT PRIMARY KEY,
-  TableName VARCHAR(100),
-  VerificationStatus ENUM('Verification Needed','Approved','Returned'),
-  SearchURL VARCHAR(255),
-  VerifiedBy VARCHAR(100),
-  VerifiedDate DATETIME
-);
