664e21aafa83acc1712799a9646bc1e033e070f2
greg5444 <52467789+greg5444@users.noreply.github.com>
2026-01-29 23:55:42 -0600

    Delete server.js

--- server.js
+++
@@ -1,9 +0,0 @@
-
-const express=require('express');
-const path=require('path');
-const app=express();
-const PORT=process.env.PORT||3000;
-app.use(express.static(path.join(__dirname,'public')));
-const uploadRouter=require('./backend/upload');
-app.use('/upload',uploadRouter);
-app.listen(PORT,()=>console.log('Apollo One running on port '+PORT));
