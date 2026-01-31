778071d3d6915ba404ecb823c272997c4177f210
Greg Waldrop <greg544@gmail.com>
2026-01-30 22:00:49 -0600

    Add navigation and placeholder views for all main pages (Bands, Concerts, Festivals, Camps, Jams, Learn)

--- src/routes/web.ts
+++ src/routes/web.ts
@@ -7,6 +7,25 @@ const listingsController = new ListingsController();
 export function setWebRoutes(app) {
     router.get('/', listingsController.getAllListings);
     router.get('/listing/:id', listingsController.getListingById);
+
+    router.get('/bands', (req, res) => {
+        res.render('bands');
+    });
+    router.get('/concerts', (req, res) => {
+        res.render('concerts');
+    });
+    router.get('/festivals', (req, res) => {
+        res.render('festivals');
+    });
+    router.get('/camps', (req, res) => {
+        res.render('camps');
+    });
+    router.get('/jams', (req, res) => {
+        res.render('jams');
+    });
+    router.get('/learn', (req, res) => {
+        res.render('learn');
+    });
     router.get('/about', (req, res) => {
         res.render('about');
     });
