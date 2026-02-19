1219cdb15074991c1fd5bda5c9e33c9126bd709f
Greg Waldrop <greg544@gmail.com>
2026-02-19 01:03:09 -0600

    fix: remove import.meta.url, use CJS built-in __dirname

--- src/server.js
+++ src/server.js
@@ -4,10 +4,6 @@ import session from 'express-session';
 import engine from 'ejs-mate';
 import dotenv from 'dotenv';
 import path from 'path';
-import { fileURLToPath } from 'url';
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
 import bandRoutes from './routes/bandRoutes.js';
 import venueRoutes from './routes/venueRoutes.js';
 import concertRoutes from './routes/concertRoutes.js';
